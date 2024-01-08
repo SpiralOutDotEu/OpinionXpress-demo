1bc663d7139aabafbd602f6edc29bddb29b29d3d
Jumaru <spiralout.eu@gmail.com>
2024-01-08 16:18:32 +0200

    display proof component

--- apps/web-app/src/pages/demo/index.tsx
+++ apps/web-app/src/pages/demo/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import IdentityComponent from './IdentityComponent';
 import OpinionXpressGroupsComponent from './OpinionXpressGroupsComponent';
+import ProofsComponent from './proofsComponents';
 
 export default function index(){
   return (
     <div>
       <IdentityComponent />
       <OpinionXpressGroupsComponent />
+      <ProofsComponent />
     </div>
   );
 };
