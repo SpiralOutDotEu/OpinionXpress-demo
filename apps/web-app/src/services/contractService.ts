a79b9d66192c9b542c55b9aac0a967d47691e0b6
Jumaru <spiralout.eu@gmail.com>
2024-04-19 16:16:28 +0300

    add survey supporting functions

--- apps/web-app/src/services/contractService.ts
+++ apps/web-app/src/services/contractService.ts
@@ -56,7 +56,6 @@ export async function getPollsCreatedEvents() {
     }))
 }
 
-
 export async function getGroupMembers(groupId: string) {
     const semaphoreEthers = new SemaphoreEthers(NETWORK_RPC, {
         address: SEMAPHORE_ADDRESS,
@@ -88,6 +87,73 @@ export async function castVote(
 
     try {
         const tx = await opinionXpress.castVote(vote, merkleTreeRoot, nullifierHash, pollId, proof, externalNullifier)
+        const mined = await tx.wait()
+        return mined.transactionHash
+    } catch (error) {
+        if (error instanceof Error) throw new Error(error.message)
+        else throw new Error("Error in transaction")
+    }
+}
+
+export async function getSurveyCreatedEvents() {
+    // Connect to an Ethereum provider
+    const provider = new ethers.providers.JsonRpcProvider(NETWORK_RPC)
+
+    // Create a new instance of the contract
+    const contract = new ethers.Contract(OPINION_X_PRESS_CONTRACT_ADDRESS as string, opinionXpressABI.abi, provider)
+
+    // Fetch the PollCreated events
+    const events = await contract.queryFilter("SurveyCreated")
+
+    return events.map((event) => ({
+        surveyId: event.args?.surveyId.toString(),
+        ipfsLink: event.args?.ipfsLink.toString(),
+        questionsCount: event.args?.questionsCount.toString(),
+        optionsPerQuestion: event.args?.optionsPerQuestion.toString()
+    }))
+}
+
+export async function getSurvey(surveyId: string) {
+    // Connect to an Ethereum provider
+    const provider = new ethers.providers.JsonRpcProvider(NETWORK_RPC)
+
+    // Create a new instance of the contract
+    const contract = new ethers.Contract(OPINION_X_PRESS_CONTRACT_ADDRESS as string, opinionXpressABI.abi, provider)
+
+    // Fetch a survey
+    const survey = await contract.getSurvey(BigInt(surveyId))
+
+    return survey
+}
+
+export async function submitSurveyResponse(
+    surveyId: number,
+    encodedResponses: BigInt,
+    merkleTreeRoot: number,
+    nullifierHash: number,
+    proof: number[],
+    groupId: number
+) {
+    const credentials = {
+        relayerApiKey: RELAYER_API_KEY,
+        relayerApiSecret: RELAYER_API_SECRET
+    }
+    const client = new Defender(credentials)
+
+    const provider = client.relaySigner.getProvider()
+    const signer = client.relaySigner.getSigner(provider, { speed: "fast" })
+
+    const opinionXpress = new ethers.Contract(OPINION_X_PRESS_CONTRACT_ADDRESS as string, opinionXpressABI.abi, signer)
+    console.log("transaction data: ", surveyId, encodedResponses, merkleTreeRoot, nullifierHash, proof, groupId)
+    try {
+        const tx = await opinionXpress.submitSurveyResponse(
+            surveyId,
+            encodedResponses,
+            merkleTreeRoot,
+            nullifierHash,
+            proof,
+            groupId
+        )
         const mined = await tx.wait()
         return mined.transactionHash
     } catch (error) {
