98988ea3c3f5888d00139c330ce7df3ed7005d97
Jumaru <spiralout.eu@gmail.com>
2024-04-20 18:40:49 +0300

    set main layout

--- apps/web-app/src/pages/_app.tsx
+++ apps/web-app/src/pages/_app.tsx
@@ -1,30 +1,16 @@
 import "../styles/globals.css"
-import '../styles/custom.css';
+import "../styles/custom.css"
 import type { AppProps } from "next/app"
-import { useEffect, useState } from "react"
-import LogsContext from "../context/LogsContext"
-import SemaphoreContext from "../context/SemaphoreContext"
-import useSemaphore from "../hooks/useSemaphore"
+
+import Header from "../components/Header"
+import BetaBanner from "../components/BetaBanner"
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    const semaphore = useSemaphore()
-    const [_logs, setLogs] = useState<string>("")
-
-    useEffect(() => {
-        semaphore.refreshUsers()
-        semaphore.refreshFeedback()
-    }, [])
-
     return (
-            <SemaphoreContext.Provider value={semaphore}>
-                <LogsContext.Provider
-                    value={{
-                        _logs,
-                        setLogs
-                    }}
-                >
-                    <Component {...pageProps} />
-                </LogsContext.Provider>
-            </SemaphoreContext.Provider>
+        <>
+            <BetaBanner />
+            <Header />
+            <Component {...pageProps} />
+        </>
     )
 }
