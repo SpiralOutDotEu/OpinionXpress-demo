c06775b17c64c4b9cdadcb857040a3822f22b850
Jumaru <spiralout.eu@gmail.com>
2024-04-20 18:38:11 +0300

    add basic header

---
+++ apps/web-app/src/components/Header.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { FaBars, FaShieldAlt } from 'react-icons/fa';
+import Link from 'next/link'
+import styles from '../styles/Header.module.css';
+
+const Header: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.brandContainer}>
+                <FaShieldAlt className={styles.brandIcon} />
+                <div className={styles.logo}>OpinionTrust</div>
+            </div>
+            <button className={styles.menuButton} onClick={handleMenuClick}>
+                <FaBars size={24} />
+            </button>
+            {isMenuOpen && (
+                <div className={styles.dropdownMenu}>
+                    <Link href="/profile/upload" className={styles.menuItem}>Identity</Link>
+                    <Link href="/demo/surveys/responses/0" className={styles.menuItem}>Polls</Link>
+                </div>
+            )}
+        </header>
+    );
+};
+
+export default Header;
