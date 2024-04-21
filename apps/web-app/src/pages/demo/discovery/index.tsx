7671556ee079ad5a5c1e237317a701f5a4d21f61
Jumaru <spiralout.eu@gmail.com>
2024-04-21 20:25:20 +0300

    convert to discovery component

--- apps/web-app/src/pages/demo/discovery/index.tsx
+++ apps/web-app/src/pages/demo/discovery/index.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import Link from "next/link"
 import NotificationBar from "../../../components/NotificationBar"
 import useIdentity from "../../../hooks/useIdentity"
+import styles from "../../../styles/DiscoverySection.module.css"
 
-export default function Upload() {
+export default function Discovery() {
     const { isLoading, notification, resetIdentity } = useIdentity()
 
     return (
@@ -13,28 +15,36 @@ export default function Upload() {
                     <div className="loading-spinner"></div>
                 </div>
             )}
-            <div className="flex flex-1 flex-col justify-center px-6 pt-8 lg:px-8 text-center">
-                <h1 className="text-4xl font-bold text-gray-900 mb-6">Discover OpinionTrust Beta</h1>
-                <p className="text-lg text-gray-600 mb-4">
+            <div className={styles.discoverySection}>
+                <h1 className={styles.title}>Discover OpinionTrust Beta</h1>
+                <p className={styles.description}>
                     You've accessed an exclusive link for early testing of OpinionTrust. Here, you can influence
                     decisions with your feedback while staying anonymous.
                 </p>
-                <p className="text-lg text-gray-600 mb-4">
+                <p className={styles.description}>
                     A unique identity is created for you, kept private in your browser. With it, you can participate in
                     votes, petitions, and surveys. This identity is yours to control - reset it anytime.
                 </p>
-                <p className="text-lg text-gray-600 mb-4">
+                <p className={styles.description}>
                     Our 'zero knowledge' encryption ensures your anonymity is never compromised, and blockchain
                     technology transparently records your input.
                 </p>
-                <p className="text-lg text-green-600 font-semibold mb-4">
+                <p className={styles.specialNote}>
                     No crypto wallet or tokens needed! Engage with blockchain seamlessly - everything is managed in our
                     unique process.
                 </p>
-                <button
-                    className="m-6 rounded bg-red-500 px-6 py-3 text-xl font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-                    onClick={resetIdentity}
-                >
+                <div className={styles.linksContainer}>
+                    <p className={styles.linkDescription}>Start making an impact now:</p>
+                    <div className={styles.links}>
+                        <Link className={styles.linkButton} href="/demo/polls">
+                            Participate in Polls
+                        </Link>
+                        <Link className={styles.linkButton} href="/demo/surveys">
+                            Engage in Surveys
+                        </Link>
+                    </div>
+                </div>
+                <button className={styles.resetButton} onClick={resetIdentity}>
                     Reset My Identity
                 </button>
             </div>
